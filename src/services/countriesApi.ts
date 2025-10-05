16b58d633f22d4cb2884d09325b2a64c451f57d8
Arkin-A <a.akcukur@lila-apfel.de>
2025-10-05 14:16:36 +0200

    style(api): apply consistent formatting to countriesApi.ts

--- src/services/countriesApi.ts
+++ src/services/countriesApi.ts
@@ -1,4 +1,4 @@
-import type { Country } from '@/types/country'
+import type {Country} from '@/types/country'
 
 const ENDPOINT = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population,cca2,cca3'
 
@@ -15,10 +15,10 @@ const ENDPOINT = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,r
  */
 
 export async function fetchCountries(): Promise<Country[]> {
-    const res = await fetch(ENDPOINT, { headers: { Accept: 'application/json' } })
+    const res = await fetch(ENDPOINT, {headers: {Accept: 'application/json'}})
     if (!res.ok) throw new Error(`HTTP ${res.status}`)
     const data = (await res.json()) as Country[]
     return data
-        .map(c => ({ ...c, capital: c.capital ?? [] }))
+        .map(c => ({...c, capital: c.capital ?? []}))
         .sort((a, b) => a.name.common.localeCompare(b.name.common))
 }
