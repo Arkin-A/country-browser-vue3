6c3f87f5620c22487847958b74e4a14f1a1ef3a9
Arkin-A <a.akcukur@lila-apfel.de>
2025-10-05 09:49:04 +0200

    chore: remove Vue template components and icons; clean App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="min-h-screen bg-gray-50 text-slate-900 flex flex-col items-center p-6">
+    <header class="w-full max-w-4xl text-center mb-8">
+      <h1 class="text-2xl font-semibold">Country Browser</h1>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="w-full max-w-4xl">
+      <!-- TODO: CountryList component -->
+      <p class="text-gray-500 italic text-center">
+        Content ...
+      </p>
+    </main>
+  </div>
 </template>
 
+<script setup lang="ts">
+// TODO: components
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
